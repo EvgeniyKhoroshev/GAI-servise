1dd371107cae5c615d6b3b86c5afd20dad2c9bb9
EvgeniyKhoroshev <sonik1995@gmail.com>
2016-09-04 20:12:22 +0300

    too many fails =(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,13 @@
 class PostsController < ApplicationController
   def show
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
+  end
+  def new
   end
   def create
     @post = Post.new(post_params)
     @post.save
-    redirect_to @post
+    redirect_to :posts => :show
   end
 
   private
